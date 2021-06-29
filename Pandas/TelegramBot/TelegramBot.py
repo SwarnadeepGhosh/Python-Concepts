350e4e3e4b33b5b1733a3a0d1adc1e23650ac190
Swarnadeep Ghosh <swarna.lyf@gmail.com>
2021-06-30 00:45:44 +0530

    Added Java Program for Telegram Bot

--- Pandas/TelegramBot/TelegramBot.py
+++ Pandas/TelegramBot/TelegramBot.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 # # Sending Data to Telegram
 
 import requests
@@ -12,12 +11,9 @@ def telegram_bot_sendtext(bot_message,bot_chatID):
     bot_token = '1811972683:AAH8RHie2JXsHsiJMhShhkQaHEr0C-gEvsA'
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
     response = requests.get(send_text)
-    
+
     return response.json()
 
-
-# test = telegram_bot_sendtext("Testing Telegram bot")
-# print(test)
 
 # df = pd.read_excel (r'D:\COURSE Modules\My Courses\Python Concepts\Pandas\TelegramBot\ekyc_data.xlsx')
 # print(df)
@@ -32,20 +28,21 @@ print(myList)
 # myList[1][1]
 # len(myList)
 
-message = ''
+message = ""
 
 for item in range(len(myList)):
     for j in range(len(myList[item])):
         message += str(myList[item][j]) + " "
     message += '\n'
 
+message = "Testing Data \n" + message
 
 test = telegram_bot_sendtext(message, '960919064' )
 print(test)
 
 
-test = telegram_bot_sendtext(message, '1038370384' )
-print(test)
+#test = telegram_bot_sendtext(message, '1038370384' )
+#print(test) #baban
 
-test = telegram_bot_sendtext(message, '406661666' )
-print(test)
+#test = telegram_bot_sendtext(message, '406661666' )
+#print(test) #rishabh bhaiya
